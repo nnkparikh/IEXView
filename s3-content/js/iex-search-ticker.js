4e5bf5a08300b2d57a289c1e25f8962371fe809d
Neel Parikh <nnkparikh@gmail.com>
2018-09-27 20:08:04 -0600

    display ticker search results.

--- s3-content/js/iex-search-ticker.js
+++ s3-content/js/iex-search-ticker.js
@@ -5,8 +5,12 @@ iex.search_ticker = (function(){
              + '<div class="main-tool-bar">'
                  + '<div class="search-bar">'
                      + '<i class="material-icons md-24">search</i>'
-                     + '<input type="text" class="search-input" placeholder="Search by ticker symbol or company." \
-                        pattern="[a-zA-Z0-9]+"/>'
+                     + '<div class="search">'
+                        + '<input type="text" class="search-input" placeholder="Search by ticker symbol or company." \
+                            pattern="[a-zA-Z0-9]+"/>'
+                        + '<div style="overflow-y: scroll; height:400px;" class="search-results">'
+                        + '</div>'
+                     + '</div>'
                  + '</div>'
                  + '<div class="active-stocks"></div>'
              + '</div>'
@@ -15,37 +19,73 @@ iex.search_ticker = (function(){
     },
     stateMap = {
         container: null,
-        symbols: null
+        symbols: []
     }, 
     elementMap = {}, 
-    setElementMap,  getSymbols,
+    setElementMap,  getSymbols, onTickerSearchEvent,
     initModule;
 
     // Event handlers
     onTickerSearchEvent = function(event){
-        console.log(this.value);
+        var result_div;
+        search_term = this.value;
+        symbols = stateMap.symbols;
+        removeSearchResults(elementMap.search_results);
+        for (var i = 0; i < symbols.length; i++){
+            var ticker = symbols[i].ticker;
+            var name = symbols[i].name;
+            var ticker_substr = ticker.substr(0, search_term.length);
+            var name_substr = name.substr(0, search_term.length);
+            var name_match = (name_substr.toLowerCase() == search_term.toLowerCase());
+            var ticker_match = (ticker_substr.toLowerCase() == search_term.toLowerCase());
+            if (name_match || ticker_match){
+                var search_results = elementMap.search_results;
+                result_div = document.createElement("div");
+                result_div.innerHTML = name;
+                search_results.append(result_div);
+            }
+        }
     };
 
     // DOM methods
+    removeSearchResults = function(element){
+        while (element.firstChild){
+            element.removeChild(element.firstChild);
+        }
+    }
+
     getSymbols = function(symbolsUrl){
         console.log("getting symbols from: " + symbolsUrl);
-        return "list of symbols..";
+        fetch(symbolsUrl)
+        .then(function(e){
+            return e.json();
+        })
+        .then(function(e){
+            e.forEach(function(e){
+                var item = {
+                    ticker: e.symbol, 
+                    name: e.name
+                };
+                stateMap.symbols.push(item);
+            });
+        });
     };
 
     setElementMap = function(){
         var container = stateMap.container;
         elementMap = {
             container: container,
-            search_input: container.querySelector('.search-input')
+            search_input: container.querySelector('.search-input'),
+            search_results: container.querySelector('.search-results')
         };
     };
 
     // Public methods
     initModule = function(container){
         stateMap.container = container;
-        stateMap.symbols = getSymbols(configMap.symbolsUrl);
         container.innerHTML = configMap.main_html;
         setElementMap();
+        getSymbols(configMap.symbolsUrl);
         var search_input = elementMap.search_input;
         search_input.addEventListener("input", onTickerSearchEvent);
     };
