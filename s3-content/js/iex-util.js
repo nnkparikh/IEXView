5a6ca46695685c6eca114eb971776ffc8901d8ae
Neel Parikh <nnkparikh@gmail.com>
2019-01-13 10:19:55 -0700

    makeError utility function added.

--- s3-content/js/iex-util.js
+++ s3-content/js/iex-util.js
@@ -1,3 +1,13 @@
 iex.util = (function(){
-	return {};
+	var makeError;
+	makeError = function(name, message, data){
+		var error = new Error();
+		error.name = name;
+		error.message = message;
+		if (data){ error.data = data; }
+		return error;
+	}
+	return {
+		makeError: makeError
+	};
 }());
